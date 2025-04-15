b82ec5eebff8138582204240da8e54a2d795bde7
Maverickrd007 <raghav.duggal.s69@kalvium.community>
2025-04-15 10:11:46 +0530

    CA DONE

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+const entityRoutes = require("./routes");
+
+app.use(express.json());
+app.use("/", routes);
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello, server is running.</h1>");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ routes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/user",async(req, res)=>{
+    try{
+        const(user) = req.body;
+        if(!user){
+            return res.status(400).json({message:"User parameter cannot be empty"});
+        }
+        return res.status(201).json({message:"user created"});
+    }catch(err){
+        return res.status(500).json({message:"Internal server error"});
+    }    
+});
+router.get("/user/:id", async(req,res)=>{
+    try{
+
+        const{id} = req.params;
+        return res.status(201).json({message:"User Found"});
+    }catch(err){
+        return res.status(404).json({message:"User not Found!"});
+    }
+});
+module.exports = router;
